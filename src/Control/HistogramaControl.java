6313acb30060f626413d12ab6def1db70baa5f5e
usuario <usuario@10.22.147.120>
2013-11-07 16:53:49 +0000

    modelo y persistencai,vista control clase dialogo

--- src/Control/HistogramaControl.java
+++ src/Control/HistogramaControl.java
@@ -8,6 +8,7 @@ import Persistencia.cargador;
 import Vista.dialogo;
 import vista.Visor;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import modelo.ConstruirHitograma;
 
 /**
@@ -16,14 +17,14 @@ import modelo.ConstruirHitograma;
  */
 public class HistogramaControl {
    
-    public void excecute() throws FileNotFoundException{
+    public void excecute() throws FileNotFoundException, IOException{
    ConstruirHitograma<String> construrHisto = new ConstruirHitograma<>();
    cargador c=new cargador();
    construrHisto.calcularHistograma(c.cargadorCorreo("C:\\Users\\usuario\\Documents"
             + "\\NetBeansProjects\\kata3Histograma\\src\\Persistencia\\misCorreos.txt"));
     Visor viewer=new Visor(construrHisto.getHistograma());  
-  //   viewer.show();
-    dialogo d=new dialogo();
-     d.execute();
+   viewer.show();
+//    dialogo d=new dialogo();
+//     d.execute();
     }
 }
